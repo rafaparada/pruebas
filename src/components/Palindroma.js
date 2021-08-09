e1ceddbc70d2d21c35dacdc308999c369a8b9c15
adalberto_parada <rafa.adalberto@gmail.com>
2021-08-09 15:34:02 -0600

    Cambiando nombre de funcion

--- src/components/Palindroma.js
+++ src/components/Palindroma.js
@@ -2,11 +2,13 @@ import React,{useState} from 'react';
 import usePalindroma from '../hooks/usePalindroma';
 const Palindroma = () =>{
     const [esPalindroma,verificarSiEsPalindroma,miWord,emptyString] = usePalindroma();
-    const demo = e =>{
+    
+    const comprobar = e =>{
         if(e.key === "Enter"){
             verificarSiEsPalindroma(e.target.value);
         }
     }
+
     return(
         <>
             <div className="container">
@@ -15,7 +17,7 @@ const Palindroma = () =>{
                     <div className="col-3"></div>
                     <div className="col-6 text-center">
                         <h3>VERIFICAR PALINDROMAS</h3>
-                        <input type="text" className="form-control" placeholder="ESCRIBA UNA PALABRA, LUEGO ENTER" onKeyUp = {demo} style={{textTransform:'uppercase'}} />
+                        <input type="text" className="form-control" placeholder="ESCRIBA UNA PALABRA, LUEGO ENTER" onKeyUp = {comprobar} style={{textTransform:'uppercase'}} />
                         <hr />
                         <h4 className = {!emptyString ? esPalindroma ? "alert alert-success":"alert alert-danger":""}>{!emptyString ? esPalindroma ? miWord+ " ES PALINDROMA" : miWord+" NO ES PALINDROMA":""}</h4>
                     </div>
